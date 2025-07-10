3b99a948b61619a696763427cea010b1cc50c8c3
ZahraaMohd-c <202203702@stu.uob.edu.bh>
2025-07-10 20:08:21 +0300

    ubdate css and made the inner squares

--- script/app.js
+++ script/app.js
@@ -1,6 +1,20 @@
 function init (){
+const squareElm=document.querySelectorAll('.square')
 
-
+function createGrid(){
+   for(let j=0; j<squareElm.length ;j++){
+    
+        for(let i=0; i<squareElm.length ;i++){
+            const inSquare= document.createElement('div')
+            inSquare.classList.add('innerSquare')
+            inSquare.id=i
+            squareElm[j].appendChild(inSquare)
+        }
+   }
+        
+   
+}
+createGrid()
 
 
 }
